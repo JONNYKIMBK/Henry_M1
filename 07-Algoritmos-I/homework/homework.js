c952360fc2553acb718d21350556cb99e9b817a2
JONNYKIMBK <jonnykimbk@hotmail.com>
2022-08-23 22:59:22 -0300

    Tarea en proceso

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,15 +6,52 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  var arr =[];
+  var factor=2;
+  var numero=num;
+
+  function factoreo(arr, factor){
+    if(numero === 1){
+      arr.unshift(1);
+    }
+    
+    if (numero%factor===0){
+      arr.push(factor);
+      numero=numero/factor;
+      return factoreo(arr,factor);
+    }
+    if(numero%factor !==0 && numero>1){
+      factor++;
+      return factoreo(arr, factor);
+    
+    }
+    
+        
+    return arr;
+
+  }
+  
+  return factoreo(arr, factor);
 
 }
+  
+
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i=0;i<array.length;i++){
+    for (let j=0;j<array.length-i-1;j++){
+      if (array[j]>array[j+1]){
+        let temp = array[j];
+        array[j]=array[j+1];
+        array[j+1]=temp;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +60,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    let j=i-1;
+    let aux=array[i];
+    while (j>=0 && array[j]>aux) {
+      array[j+1]=array[j];
+      --j;
+    }
+    array[j+1]=aux;
+  }
+  return array;
 }
 
 
